674115667b4f04c19a5bc49d18ff13555d5f9417
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-10-12 00:56:05 +0900

    함수 재정리

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 'use strict';
 import PopUp from './popup.js';
+import * as sound from './sound.js';
 import { GameBuilder, Reason } from './game.js';
 
 const gameFinishBanner = new PopUp();
@@ -14,12 +15,15 @@ game.setGameStopListener((reason) => {
   switch (reason) {
     case Reason.cancel:
       message = 'Replay?';
+      sound.playAlert();
       break;
     case Reason.win:
       message = 'You Win !';
+      sound.playWin();
       break;
     case Reason.lose:
       message = 'You Lost !';
+      sound.playBug();
       break;
     default:
       throw new Error('not valid reason');
